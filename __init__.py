ac4d2f794807a466a28cdc3a55850b4117a08269
dkolas <4643013+dkolas@users.noreply.github.com>
2023-06-20 08:58:18 -0400

    Make calls to Steamship

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
 # TODO: Add an appropriate license to your skill before publishing.  See
 # the LICENSE file for more information.
-
+import requests
 from mycroft.skills.core import FallbackSkill
+from mycroft.skills.audioservice import AudioService
 from mycroft.util.log import LOG
+import time
 
 
 class SteamshipAgentSkill(FallbackSkill):
@@ -10,7 +12,7 @@ class SteamshipAgentSkill(FallbackSkill):
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
         super(SteamshipAgentSkill, self).__init__(name="SteamshipAgentSkill")
-        
+
     def initialize(self):
         """Registers the fallback skill."""
         # Warning:
@@ -20,13 +22,29 @@ class SteamshipAgentSkill(FallbackSkill):
         # a fallback prio between 11 and 89 is good for most skills.
 
         self.register_fallback(self.handle_fallback, 1)
+        self.audio_service = AudioService(self.bus)
 
         # Any other initialize code goes here
 
     def handle_fallback(self, message):
         utterance = message.data.get("utterance")
 
-        self.speak('Echoing a test: '+utterance)
+        api_key = self.settings.get('steamship_api_key')
+        invocation_url = self.settings.get('package_instance_invocation_url') + '/mycroft_respond'
+        user_id = self.settings.get('user_id')
+
+        result = requests.post(invocation_url, headers={"Authorization":f"Bearer {api_key}"}, json={"user_id":user_id, "message":utterance})
+        voice_url = result.text
+        self.log.info(f"Got result URL: {voice_url}")
+        response = requests.get(voice_url, headers={"Authorization":f"Bearer {api_key}"})
+        filename = time.strftime("%Y%m%d-%H%M%S") + ".mp3"
+        file_path = self.file_system.path.absolute() / filename
+
+        self.log.info(f"Saved data at: {file_path}")
+        with self.file_system.open(filename, "wb") as f:
+            f.write(response.content)
+
+        self.audioservice.play(tracks=(f"file://{file_path}", "audio/mpeg"))
 
         return True  # Indicate that the utterance was handled
 
